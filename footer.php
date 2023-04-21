b91c2efdcd1e7bbe442338b1f6464a64419ddf4f
Andrej <82274090+andrej-gorochko@users.noreply.github.com>
2023-04-21 08:45:42 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,5 +3,13 @@
     <p class="text-center text-muted"><a class="nav-link" href="https://goroshcko.ru/">© 2021 goroshcko.ru , Inc</a></p>
   </footer>
 </div>
+<script>
+  $(document).ready(function () {
+    // открытие/закрытие меню
+    $('.navbar-toggler').click(function () {
+      $('.navbar-collapse').toggleClass('show');
+    });
+  });
+</script>
 </body>
 </html>
